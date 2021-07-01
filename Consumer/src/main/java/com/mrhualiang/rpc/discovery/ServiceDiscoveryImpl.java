c3fb8920867e37681f6c2a1a6bd94a08be020a4f
zhuhualiang275 <zhuhualiang275@hellobike.com>
2021-07-01 19:32:38 +0800

    error修复

--- Consumer/src/main/java/com/mrhualiang/rpc/discovery/ServiceDiscoveryImpl.java
+++ Consumer/src/main/java/com/mrhualiang/rpc/discovery/ServiceDiscoveryImpl.java
@@ -12,7 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
-
+import com.mrhualiang.rpc.config.*;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
