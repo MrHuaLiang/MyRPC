d20a6e3300acd57df383be6cddc45acf9750f9a4
zhuhualiang275 <zhuhualiang275@hellobike.com>
2021-07-01 19:20:18 +0800

    项目结构优化

--- Consumer/src/main/java/com/mrhualiang/rpc/discovery/ServiceDiscoveryImpl.java
+++ Consumer/src/main/java/com/mrhualiang/rpc/discovery/ServiceDiscoveryImpl.java
@@ -12,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
-import com.mrhualiang.rpc.config.*;
 
 import java.util.HashMap;
 import java.util.List;
