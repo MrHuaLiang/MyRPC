7323893dcdafb3ce6c2c2c1c9c81b7e8ac76ee77
zhuhualiang275 <zhuhualiang275@hellobike.com>
2021-07-03 12:55:46 +0800

    服务注销

--- Common/src/main/java/com/mrhualiang/rpc/threadPool/MyScheduledThreadPoolExecutor.java
+++ Common/src/main/java/com/mrhualiang/rpc/threadPool/MyScheduledThreadPoolExecutor.java
@@ -1,10 +1,12 @@
 package com.mrhualiang.rpc.threadPool;
 
-import java.util.concurrent.ScheduledThreadPoolExecutor;
-import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.*;
 
 import static java.util.concurrent.TimeUnit.NANOSECONDS;
 
-public class MyScheduledThreadPoolExecutor {
+public class MyScheduledThreadPoolExecutor extends ThreadPoolExecutor {
 
+    public MyScheduledThreadPoolExecutor(int corePoolSize, int maximumPoolSizer) {
+        super(corePoolSize, maximumPoolSizer, 0, TimeUnit.MILLISECONDS, new DelayQueue<>(), Executors.defaultThreadFactory(), new DiscardPolicy());
+    }
 }
